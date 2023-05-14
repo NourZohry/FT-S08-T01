52c90bcb5065153cc467f7c5b2cc716847d679c7
NourZohry <nzohry@gmail.com>
2023-05-14 16:08:03 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,128 @@
+let cardsWrapper = document.getElementById("cards-wrapper");
+cardsWrapper.innerHTML = `<div class="row">
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-1.png" alt="Card icon">
+    <h1>اﻟﺄﻓﻀﻞ ﻣﺸﻮرة</h1>
+    <p>اﻧﺘﻘﺎء اﻟﻤﻨﺘﺠﺎت اﻟﺄﻓﻀﻞ. اﻟﻤﺴﺘﺜﻤﺮ ﻋﻠﻰ ﺗﻜﻮﻳﻦ ﻣﺤﺎﻓﻆ ﻣﺘﻨﻮﻋﺔ وﻋﻠﻰ اﻟﻌﺪﻳﺪ ﻣﻦ اﻟﺂﻟﻴﺎت اﻟﺤﺼﺮﻳﺔ اﻟﺘﻲ ﺗﺴﺎﻋﺪ ﺑﺎﻟﺈﺿﺎﻓﺔ إﻟﻰ اﺳﺘﻘﻠﺎﻟﻴﺘﻨﺎ ﻓﻲ دراﻳﺔ، ﻓﻘﺪ ﻃﻮرﻧﺎ</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-2.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ راﺣﺔ</h1>
+    <p>ﻋﺒﺮ ﻣﻮﻗﻊ واﺣﺪ وﺣﺴﺎب واﺣﺪ. ﺑﻞ أﺻﺒﺢ ﺑﺈﻣﻜﺎﻧﻚ ﻣﺘﺎﺑﻌﺔ ﻛﻞ اﺳﺘﺜﻤﺎراﺗﻚ وإدارﺗﻬﺎ ﻓﻲ ﻛﺎﻓﺔ اﻟﻤﻨﺘﺠﺎت اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ ﻣﻦ ﻣﻜﺎن واﺣﺪ، أﺳﻤﺎء دﺧﻮل ﻣﺨﺘﻠﻔﺔ ﻟﺄن دراﻳﺔ ﺗﺘﻴﺢ ﻟﻚ اﻟﺎﺳﺘﺜﻤﺎر ﻟﻦ ﺗﺤﺘﺎج إﻟﻰ ﻓﺘﺢ ﺣﺴﺎﺑﺎت ﻣﺘﻌﺪدة؛ أو ﺣﻔﻆ</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-3.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ ﺗﻨﻮﻳﻌًﺎ</h1>
+    <p>المؤشرات المتداولة FTE التداول في ما يزيد عن 002,1 من صناديق والصكوك، والمرابحة...إلخ( والأسواق كما يمكنك تستثمر في كافة الفئات الاستثمارية )كالأسهم، استثماري من 01 مدراء مختلفين. هذه المنتجات تمكنك دراية من الاستثمار في أكثر من 06 منتج</p>
+  </div>
+</div>
+
+</div>
+<div class="row">
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-4.png" alt="Card icon">
+    <h1>وﺑﻠﺎ ﺗﻜﻠﻔﺔ إﺿﺎﻓﻴﺔ</h1>
+    <p>ﻣﺒﺎﺷﺮة (أي ﺑﺪون أي رﺳﻮم إﺿﺎﻓﻴﺔ). اﻟﻤﺘﺎﺣﺔ ﻋﻨﺪ اﻟﺎﺳﺘﺜﻤﺎر ﻣﻊ اﻟﺠﻬﺔ اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ ﻛﺎﻓﺔ اﻟﻤﻨﺘﺠﺎت اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ ﻣﺘﺎﺣﺔ ﺑﻨﻔﺲ اﻟﺘﻜﻠﻔﺔ</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-5.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ ﺷﻔﺎﻓﻴﺔ</h1>
+    <p>ﺷﻔﺎف ﻟﺎﻧﺘﻘﺎء اﻟﺄﻓﻀﻞ ﻣﻦ ﺑﻴﻨﻬﺎ. اﻟﺘﻲ ﺗﻘﺪﻣﻬﺎ دراﻳﺔ ﻣﻦ ﺧﻠﺎل ﺣﺴﺎب ﻣﻮﺣﺪ ﺑﺸﻜﻞ أﺻﺒﺢ ﻣﻦ اﻟﺴﻬﻞ ﻋﻠﻴﻚ اﻟﻤﻘﺎرﻧﺔ ﺑﻴﻦ اﻟﻤﻨﺘﺠﺎت</p>
+  </div>
+</div>
+
+<div class="col-md-4 gy-4">
+  <div class="card h-100 w-100 shadow-sm">
+    <img src="assets/images/card-6.png" alt="Card icon">
+    <h1>اﻟﺄﻛﺜﺮ اﺳﺘﻘﻠﺎﻟﻴﺔ</h1>
+    <p>اﻟﺎﺳﺘﻘﻠﺎﻟﻴﺔ واﻟﺤﻴﺎدﻳﺔ ﻓﻲ ﺗﻌﺎﻣﻠﻬﺎ وﻣﺸﻮرﺗﻬﺎ. ﺑﻌﻴﻨﻬﺎ، اﻟﺄﻣﺮ اﻟﺬي ﻳﻌﻄﻴﻬﺎ ﻗﺪر أﻛﺒﺮ ﻣﻦ ﺗﺤﺖ ﺳﻘﻒ واﺣﺪ ﺑﺪﻟﺄ ﻣﻦ اﻟﺘﺮﻛﻴﺰ ﻋﻠﻰ ﺑﻴﻊ ﻣﻨﺘﺠﺎت دراﻳﺔ ﺗﺮﻛﺰ ﻋﻠﻰ ﺟﻤﻊ ﻛﺎﻓﺔ اﻟﻤﻨﺘﺠﺎت اﻟﺎﺳﺘﺜﻤﺎرﻳﺔ</p>
+  </div>
+</div>`
+
+
+const brief = document.getElementById('brief');
+brief.innerText = trunc(brief.innerText, 300);
+
+const popupContainer = document.getElementById('popup-container');
+const popupTitle = document.getElementById('popup-title');
+const popupText = document.getElementById('popup-text');
+const popupClose = document.querySelectorAll('.popup-close');
+
+const popupDarkBG = document.getElementById('popup-dark-bg');
+
+for (let i = 0; i < popupClose.length; i++) { // For both "Close" text and "X" button
+  popupClose[i].addEventListener("click", () => {
+    document.getElementsByTagName("body")[0].style.overflowY = "scroll";
+    popupDarkBG.style.visibility = "hidden";
+    popupContainer.style.visibility = "hidden";
+  }); 
+}
+
+function changeText(h1, p) {
+  document.getElementsByTagName("body")[0].style.overflowY = "hidden";
+  popupDarkBG.style.visibility = "visible";
+  popupContainer.style.visibility = "visible";
+  popupTitle.innerText = h1;
+  popupText.innerText = p;
+}
+
+function trunc(text, amount) {
+  return text.slice(0, amount) + "...";
+}
+
+let cardsTextArray = []
+let cards = document.querySelectorAll('.card');
+for (let i = 0; i < cards.length; i++) {
+  let cardLength = cards[i].getElementsByTagName("p")[0].innerText.length;
+  if (cardLength > 120) {
+    cardsTextArray[i] = cards[i].getElementsByTagName("p")[0].innerText;
+
+    const readMoreButton = document.createElement("div");
+    readMoreButton.style.backgroundColor = "#510B76";
+    readMoreButton.style.color = "#fff";
+    readMoreButton.style.position = "absolute";
+    readMoreButton.style.bottom = "20px";
+    readMoreButton.style.paddingRight = "20px";
+    readMoreButton.style.paddingLeft = "20px";
+    readMoreButton.setAttribute('id','read-more-button');
+    readMoreButton.classList.add('background-color');
+    readMoreButton.innerText = "read more";
+    readMoreButton.addEventListener("click", () => {
+        changeText(
+          cards[i].getElementsByTagName("h1")[0].innerText,
+          cardsTextArray[i]
+        )
+    });
+
+    cards[i].style.paddingBottom = "40px";
+    cards[i].append(readMoreButton);
+    cards[i].getElementsByTagName("p")[0].innerText = trunc(cards[i].getElementsByTagName("p")[0].innerText, 120);
+  }  
+}
+
+function themePurple() {
+    const collection = document.getElementsByClassName("background-color");
+
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.backgroundColor = "#510B76";
+    }
+}
+
+function themeGreen() {
+    const collection = document.getElementsByClassName("background-color");
+
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].style.backgroundColor = "#0B760F";
+    }
+}
